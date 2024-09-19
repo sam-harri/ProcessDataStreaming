ee9e1931dc3b3fd64e5b8fde66dbc9d7a95ccc97
Sam Harrison <sharr125@uottawa.ca>
2024-09-19 18:41:56 -0400

    [feat] distillation column data generator

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -1,28 +1,24 @@
-// main.rs
-
 use heat_exchanger::HeatExchanger;
+use distillation_column::DistillationColumn;
 use tokio::task;
 
 mod producer;
 mod schema_loader;
 mod heat_exchanger;
-// mod distillation_column; // Uncomment if you have this module
+mod distillation_column;
 
 #[tokio::main]
 async fn main() {
-    let heat_exchanger = HeatExchanger::new();
-    let topic = "test-topic";
-
     let exchanger_handle = task::spawn(async move {
-        heat_exchanger.produce_data(topic).await;
+        let heat_exchanger = HeatExchanger::new();
+        heat_exchanger.produce_data("hx-topic").await;
     });
 
-    // If you have other units, instantiate and spawn them here
-    // let distillation_column = DistillationColumn::new();
-    // let column_handle = task::spawn(async move {
-    //     distillation_column.produce_data("distillation-topic").await;
-    // });
+    let column_handle = task::spawn(async move {
+        let distillation_column = DistillationColumn::new();
+        distillation_column.produce_data("distillation-topic").await;
+    });
 
     exchanger_handle.await.unwrap();
-    // column_handle.await.unwrap();
+    column_handle.await.unwrap();
 }
